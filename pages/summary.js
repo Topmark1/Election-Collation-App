4b83a025a499b51684848e66e6001042ca43da2e
Temitope Akande <107555863+Topmark1@users.noreply.github.com>
2023-02-25 19:39:53 +0100

    Update summary.js

--- pages/summary.js
+++ pages/summary.js
@@ -47,7 +47,7 @@ const summary = () => {
       return previousValue*1 + currentValue.Result.Presidential.ADC*1;
     }, 0);
 
-    LESAPC += ward.pollingUnits.reduce(function (previousValue, currentValue) {
+    LESAPC += ward.pollingUnits.reduce(function (previousValue*1, currentValue*1) {
       return previousValue + currentValue.Result.LagosEastSenatorial.APC;
     }, 0);
     });
